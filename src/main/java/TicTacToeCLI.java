3c26242555a11215c40ffe671d802e1f9f86d30d
Trey Thompson <trey.thompson413@gmail.com>
2022-10-08 01:29:23 -0500

    first commit

---
+++ src/main/java/TicTacToeCLI.java
@@ -0,0 +1,10 @@
+import application.TicTacToe;
+
+public class TicTacToeCLI {
+
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.run();
+    }
+
+}
---
+++ src/main/java/application/TicTacToe.java
@@ -0,0 +1,10 @@
+package application;
+
+public class TicTacToe {
+
+    public void run() {
+        while(true){
+
+        }
+    }
+}
